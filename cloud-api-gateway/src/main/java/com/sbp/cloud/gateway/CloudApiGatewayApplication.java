c75f475f9eb2f3e2ad48d2246044b6d5ba66f77b
Sharvil <you@example.com>
2023-07-03 10:04:23 +0530

    Implemented Hystrix

--- cloud-api-gateway/src/main/java/com/sbp/cloud/gateway/CloudApiGatewayApplication.java
+++ cloud-api-gateway/src/main/java/com/sbp/cloud/gateway/CloudApiGatewayApplication.java
@@ -3,9 +3,11 @@ package com.sbp.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class CloudApiGatewayApplication {
 
 	public static void main(String[] args) {
