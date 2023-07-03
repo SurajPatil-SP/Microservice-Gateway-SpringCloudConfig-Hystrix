0bac582bc3a06186666cd3a70c27791f6ebcc7a4
Sharvil <you@example.com>
2023-07-03 10:05:30 +0530

    Implemented WebClient using SpringWebFlux Library

--- order-service/src/main/java/com/sbp/orderservice/api/OrderServiceApplication.java
+++ order-service/src/main/java/com/sbp/orderservice/api/OrderServiceApplication.java
@@ -7,7 +7,14 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 @EntityScan
@@ -19,11 +26,32 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 	
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		RestTemplate rst = new RestTemplate();
-		return rst;
-	}
+//	@Bean
+//	@LoadBalanced
+//	public RestTemplate restTemplate() {
+//		RestTemplate rst = new RestTemplate();
+//		return rst;
+//	}
+	
+//	@Bean
+//	public WebClient webClient () {
+//		return WebClient.builder().build();
+//	}
 
+    @Bean
+    public WebClient getWebClient(WebClient.Builder webClientBuilder) {
+
+        return webClientBuilder
+            .clientConnector(new ReactorClientHttpConnector(getHttpClient()))
+            .baseUrl("http://localhost:8082/payment")
+            .build();
+    }
+
+    private HttpClient getHttpClient() {
+        return HttpClient.create()
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10_000)
+            .doOnConnected(conn -> conn
+                .addHandlerLast(new ReadTimeoutHandler(10))
+                .addHandlerLast(new WriteTimeoutHandler(10)));
+    }
 }
--- order-service/src/main/java/com/sbp/orderservice/api/service/OrderService.java
+++ order-service/src/main/java/com/sbp/orderservice/api/service/OrderService.java
@@ -1,8 +1,11 @@
 package com.sbp.orderservice.api.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.sbp.orderservice.api.dto.Payment;
 import com.sbp.orderservice.api.dto.TransactionRequest;
@@ -10,14 +13,19 @@ import com.sbp.orderservice.api.dto.TransactionResponse;
 import com.sbp.orderservice.api.entity.Order;
 import com.sbp.orderservice.api.repository.OrderRepository;
 
+import reactor.core.publisher.Mono;
+
 @Service
 public class OrderService {
 
 	@Autowired
 	private OrderRepository orderRepository;
 
+//	@Autowired
+//	private RestTemplate restTemplate;
+	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WebClient webClient;
 
 	public TransactionResponse saveOrder(TransactionRequest request) {
 		String response = "";
@@ -27,8 +35,15 @@ public class OrderService {
 		payment.setAmount(order.getPrice());
 
 		// rest call
-		Payment paymentResponse = restTemplate.postForObject("http://PAYMENT-SERVICE/payment/doPayment", payment,
-				Payment.class);
+//		Payment paymentResponse = restTemplate.postForObject("http://PAYMENT-SERVICE/payment/doPayment", payment,
+//				Payment.class);
+		
+		Payment paymentResponse = webClient.post().uri("/doPayment")
+				.body(Mono.just(payment), Payment.class)
+				.retrieve()
+				.bodyToMono(Payment.class)
+				.block();
+		
 		response = paymentResponse.getPaymentStatus().equals("success")
 				? "Payment Processed Successfully and Order Placed"
 				: "There is failure in Payment API, Order added to cart";
